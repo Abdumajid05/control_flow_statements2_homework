3d5b5607b7810bb9d778aa2c5b55b1142a8c61ac
Abdumajid05 <abdumajidabdulazizov513@gmail.com>
2024-03-06 22:25:02 +0500

    new

--- if05.py
+++ if05.py
@@ -6,4 +6,20 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a=n//10000
+    b=(n//1000)%10
+    c=(n//100)%10
+    d=(n%100)//10
+    x=(n%100)%10
+    if a>b and a>c and a>d and a>x:
+        return a
+    if b>a and b>c and b>d and b>x:
+        return b
+    if c>a and c>b and c>d and c>x:
+        return c
+    if d>a and d>b and d>c and d>x:
+        return d
+    if x>a and x>b and x>c and x>d:
+        return x
+    
+print(main(23654))
--- if06.py
+++ if06.py
@@ -6,4 +6,22 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a=n//10000
+    b=(n//1000)%10
+    c=(n//100)%10
+    d=(n%100)//10
+    x=(n%100)%10
+    if a>b and a>c and a>d and a>x:
+        return 5
+    if b>a and b>c and b>d and b>x:
+        return 4
+    if c>a and c>b and c>d and c>x:
+        return 3
+    if d>a and d>b and d>c and d>x:
+        return 2
+    if x>a and x>b and x>c and x>d:
+        return 1
+    
+    
+    
+print(main(23951))
